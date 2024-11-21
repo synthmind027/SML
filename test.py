9846ab724e2b6de942b517ff824008a7113405e7
synthmind027 <synthmind027@gmail.com>
2024-11-22 06:25:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from sml_core import sml_core
-import sml
+from sml import sml_globals
 
 a = sml_core()
 a.get('a.b')
 a.set('a.b.c','Hello, world!')
 print(a.get('a.b.c'))
+
+func = sml_globals.get('**')
+print(func)
+print(func(12,2))
